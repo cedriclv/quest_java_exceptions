ac3106c5fb6d389237680e278c4ce235aebe33a7
CEDRIC LV <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2023-03-03 16:48:20 +0100

    first commit

--- src/main/OddSum.java
+++ src/main/OddSum.java
@@ -9,15 +9,26 @@ public class OddSum {
     public static int run(String[] args, boolean test) {
 
         ArrayList<Integer> listInt = new ArrayList<>();
+        // Integer one = 1;        
+        // Integer two = 2;
+        // Integer five = 5;
+        // listInt.add(one);        
+        // listInt.add(two);        
+        // listInt.add(five);        
         int res;
         if (!test) {
+            System.out.println("Passe la");
             System.out.println("Elements' list :");
             for (String v : args)
                 System.out.print(" " + v);
             System.out.println();
         }
         for (int i = 0; i < args.length; i++) {
-            listInt.add(Integer.parseInt(args[i]));
+            try {
+                listInt.add(Integer.parseInt(args[i]));                
+            } catch (NumberFormatException e) {
+                System.err.println(e);
+            }
         }
 
         res = sum(listInt);
